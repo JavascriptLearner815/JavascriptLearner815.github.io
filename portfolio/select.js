e4b9f90e5b6bfd4ea1dc8f33c7f27b05a8937507
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-06-07 12:19:22 -0500

    Update select.js

--- portfolio/select.js
+++ portfolio/select.js
@@ -16,6 +16,7 @@ export default class Select {
 
 function setupCustomElement(select) {
   select.customElement.classList.add("__custom-select-container")
+  select.customElement.tabIndex = 0
   
   select.labelElement.classList.add("__custom-select-value")
   select.labelElement.innerText = select.selectedOption.label
